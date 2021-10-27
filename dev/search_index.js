b8ba148a432e4d67412d78bcf2f256a6bc858e9e
Documenter.jl <documenter@juliadocs.github.io>
2021-10-27 07:16:38 +0000

    build based on 4ad4ab4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChrisPkg","category":"page"},{"location":"#ChrisPkg","page":"Home","title":"ChrisPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChrisPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChrisPkg]","category":"page"}]
+}
